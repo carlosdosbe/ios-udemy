998c1a689e43d3c7aaca9be643c774943db4981a
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-03-08 13:50:08 -0600

    2

---
+++ 1-HELLOWORLD/HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Carlos Daniel OB on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
