43bef829bf1abb4922e935bf79b972b4a04daf03
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-03-09 13:12:16 -0600

    21. 1 - Restart

---
+++ 2-RESTART/RECURSOS/Restart-App/Students/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Carlos Daniel OB on 08/03/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
