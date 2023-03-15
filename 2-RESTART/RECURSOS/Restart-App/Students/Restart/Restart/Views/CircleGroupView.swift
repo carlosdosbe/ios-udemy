cd9738330a96b6a3f0a7dcbb14c2cabf18df7ff5
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-03-15 12:19:46 -0600

    21. 3. RESTART - HomeView

---
+++ 2-RESTART/RECURSOS/Restart-App/Students/Restart/Restart/Views/CircleGroupView.swift
@@ -0,0 +1,37 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Carlos Daniel OB on 15/03/23.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    // MARK: - PROPERTY
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    // MARK: - BODY
+    var body: some View {
+        ZStack{
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+struct CircleGroupView_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color("ColorGreen")
+                .ignoresSafeArea(.all, edges: .all)
+            CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        }
+    }
+}
