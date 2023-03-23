1d493cf0a42b8652240703df9c0807b0168300bf
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-03-22 18:58:16 -0600

    30. 1. PINCH - SETTINGS

---
+++ 3-PINCH/PINCH/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Carlos Daniel OB on 22/03/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
