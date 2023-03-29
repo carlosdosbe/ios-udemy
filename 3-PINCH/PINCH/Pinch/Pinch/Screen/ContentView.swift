fcf45864ec6240e30d3195acb797f4865d35cc5c
Carlos Daniel OB <carlosdosbe@gmail.com>
2023-03-28 18:16:26 -0600

    33. 4. PINCH - LONG PRESS GESTURE

--- 3-PINCH/PINCH/Pinch/Pinch/Screen/ContentView.swift
+++ 3-PINCH/PINCH/Pinch/Pinch/Screen/ContentView.swift
@@ -11,12 +11,21 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    func resetImageState(){
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+    
     // MARK: - CONTENT
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -25,6 +34,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1: 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                     // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -34,11 +44,24 @@ struct ContentView: View {
                             }
                         }
                         else{
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                    // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged{ value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded{ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                                
+                            }
+                    )
                 
             }//: ZSTACK
             .navigationTitle("Menu")
@@ -48,6 +71,14 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                , alignment: .top
+            )
+            
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
